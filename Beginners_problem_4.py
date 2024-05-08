74c032ff49290f7eced369982eb6e99fe0ea84ba
greenspook001 <160692620+greenspook001@users.noreply.github.com>
2024-05-08 14:14:42 +1200

    Update Beginners_problem_4.py

--- Beginners_problem_4.py
+++ Beginners_problem_4.py
@@ -1,3 +1,5 @@
+# My first work that I have done
+
 for attempt in range(3):
     password = input("Enter Password: ")
     if password == "Kobus":
@@ -5,3 +7,14 @@ for attempt in range(3):
         break
 else:
     print("Locked")
+
+# My second work that I have done
+
+number = int(input("Choose a number = "))
+name = input("What is your name = ")
+
+for _ in range(number):
+    print(name)
+    
+# My third work that I have done
+
